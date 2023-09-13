920e83d2e7fc0851111610022b16d8867c0f209c
Jean <JeanChirinos17@gmail.com>
2023-09-13 00:44:36 -0500

    🚧Cropper in progress

--- app/(session)/perfil/page.tsx
+++ app/(session)/perfil/page.tsx
@@ -1,64 +1,131 @@
-"use client"
-import { Button } from "@/components/Button";
-import { Input } from "@/components/Input";
-import Image from "next/image";
-import {BiSolidCamera} from "react-icons/bi";
-import {HiOutlineMail, HiOutlineDeviceMobile} from "react-icons/hi";
-import { useState } from 'react';
-import { CropperRef, Cropper, CircleStencil } from 'react-advanced-cropper';
+'use client'
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import Image from 'next/image'
+import { BiSolidCamera } from 'react-icons/bi'
+import { HiOutlineMail, HiOutlineDeviceMobile } from 'react-icons/hi'
+import { useRef, useState } from 'react'
+import { CropperRef, Cropper, CircleStencil } from 'react-advanced-cropper'
 import 'react-advanced-cropper/dist/style.css'
-import Logo from "@/public/img/logo.webp";
 
-export default function Page()
-{
-    const [image] = useState(
-        Logo.src,
-    );
+export default function Page() {
+  // REF
+  const cropperRef = useRef<CropperRef>(null)
 
-    return (
-        <div className="mx-auto w-[400px] max-w-full">
-            {/* <Cropper
-                src={image}
-                className={'cropper'}
-                stencilComponent={CircleStencil}
-            /> */}
+  // STATES
+  const [isEditable, setIsEditable] = useState(false)
+  const [isImageEditable, setIsImageEditable] = useState(false)
+  const [imagePreview, setImagePreview] = useState('')
 
-            <section className="relative w-[250px] aspect-square mx-auto max-w-full">
-                <Image className="rounded-full object-cover" src="https://api-encuentratumascota.nijui.com/users/1/profile.webp" width="600" height="600" alt="Lechonk aesthetic" />
-                <button className="absolute bottom-5 right-5 text-2xl text-white bg-pink-500 rounded-full p-1">
-                    <BiSolidCamera/>
-                </button>
-            </section>
-            <section className="flex flex-col gap-3">
-                <div>
-                    <div className="text-center">
-                        <h1 className="text-2xl font-bold leading-none">name</h1>
-                        <span>username</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                        <HiOutlineMail/> 
-                        <span>Correo: email</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                        <HiOutlineDeviceMobile/> 
-                        <span>Celular: mobile</span>
-                    </div>
-                </div>
-                <Button className="bg-primary text-white" v>Editar</Button>
-            </section>
+  // FUNCTIONS
+  function handleCancel() {
+    setIsEditable(false)
+  }
 
-            <section>
-                <form action="" className="flex flex-col gap-3">
-                    <Input type="text" label="Nombre" isRequired={false}/>
-                    <Input type="text" label="Usuario"/>
-                    {/* <Input type="text" label="Correo" isRequired={false}/> */}
-                    <Input type="text" label="Móvil" isRequired={false}/>
-                    <div className="flex gap-2 w-full child:w-1/2">
-                        <Button type="button" className="">Cancelar</Button>
-                        <Button type="submit" className="bg-primary text-white">Guardar</Button>
-                    </div>
-                </form>
-            </section>
-        </div>
-    )
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    setIsEditable(false)
+  }
+
+  function handleInputImage(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) return
+
+    const file = e.target.files[0]
+
+    if (file) {
+      setIsImageEditable(true)
+      setImagePreview(URL.createObjectURL(file))
+    }
+  }
+
+  function submitUpdateImage(e: React.FormEvent) {
+    e.preventDefault()
+    const canvas = cropperRef.current?.getCanvas()?.toDataURL()
+
+    // if (!canvas) return
+    // TODO: Send image to server
+  }
+
+  // RENDER
+  return (
+    <div className='mx-auto w-[400px] max-w-full space-y-3'>
+      <section className='relative mx-auto aspect-square w-[250px] max-w-full'>
+        {isImageEditable ? (
+          <form className='flex flex-col gap-y-2' onSubmit={submitUpdateImage}>
+            <Cropper
+              src={imagePreview}
+              className='cropper'
+              stencilComponent={CircleStencil}
+              ref={cropperRef}
+            />
+            <Button type='submit' className='bg-primary text-white'>
+              Guardar
+            </Button>
+          </form>
+        ) : (
+          <>
+            <Image
+              className='rounded-full object-cover'
+              src='https://api-encuentratumascota.nijui.com/users/1/profile.webp'
+              width='600'
+              height='600'
+              alt='Lechonk aesthetic'
+            />
+
+            <label className='absolute bottom-5 right-5 cursor-pointer'>
+              <div className='rounded-full bg-pink-500 p-1 text-2xl text-white'>
+                <BiSolidCamera />
+              </div>
+              <input
+                type='file'
+                className='invisible absolute'
+                accept='image/*'
+                onChange={handleInputImage}
+              />
+            </label>
+          </>
+        )}
+      </section>
+      {!isEditable && (
+        <section className='flex flex-col gap-3'>
+          <div>
+            <div className='text-center'>
+              <h1 className='text-2xl font-bold leading-none'>name</h1>
+              <span>username</span>
+            </div>
+            <div className='flex items-center gap-1'>
+              <HiOutlineMail />
+              <span>Correo: email</span>
+            </div>
+            <div className='flex items-center gap-1'>
+              <HiOutlineDeviceMobile />
+              <span>Celular: mobile</span>
+            </div>
+          </div>
+          <Button className='bg-primary text-white' onPress={() => setIsEditable(true)}>
+            Editar
+          </Button>
+        </section>
+      )}
+
+      {isEditable && (
+        <section>
+          <form action='' className='flex flex-col gap-3' onSubmit={handleSubmit}>
+            <Input type='text' label='Nombre' isRequired={false} />
+            <Input type='text' label='Usuario' />
+            {/* <Input type="text" label="Correo" isRequired={false}/> */}
+            <Input type='text' label='Móvil' isRequired={false} />
+            <div className='flex w-full gap-2 child:w-1/2'>
+              <Button type='button' onPress={handleCancel}>
+                Cancelar
+              </Button>
+              <Button type='submit' className='bg-primary text-white'>
+                Guardar
+              </Button>
+            </div>
+          </form>
+        </section>
+      )}
+    </div>
+  )
 }
