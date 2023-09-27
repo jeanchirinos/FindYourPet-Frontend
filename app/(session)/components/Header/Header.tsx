ca5090abfb6e88ea7ac6b3088e64be414e3b45bb
Jean <JeanChirinos17@gmail.com>
2023-09-27 16:55:53 -0500

    🐛Railway error: "url" parameter is valid but upstream response is invalid

--- app/(session)/components/Header/Header.tsx
+++ app/(session)/components/Header/Header.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Session } from './Session'
 import { Suspense } from 'react'
+import Logo from '@/public/img/logo.webp'
 
 export function Header() {
   return (
     <header className='fixed inset-0 z-20 mx-auto flex h-[40px] w-[1600px] max-w-full justify-between px-1.5'>
       <Link href='/' aria-label='Inicio'>
-        <Image src='/img/logo.webp' alt='Logo' height={40} width={40} loading='eager' />
+        <Image src={Logo} alt='Logo' width={40} loading='eager' />
       </Link>
       <Suspense>
         <Session />
