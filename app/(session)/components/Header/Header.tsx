0e6711f45e03f900a20a7ac74a3ac258dd42904b
Jean <JeanChirinos17@gmail.com>
2023-09-27 16:41:20 -0500

    🚧

--- app/(session)/components/Header/Header.tsx
+++ app/(session)/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Logo from '@/public/img/logo.webp'
 import Link from 'next/link'
 import { Session } from './Session'
 import { Suspense } from 'react'
@@ -8,7 +7,7 @@ export function Header() {
   return (
     <header className='fixed inset-0 z-20 mx-auto flex h-[40px] w-[1600px] max-w-full justify-between px-1.5'>
       <Link href='/' aria-label='Inicio'>
-        <Image src={Logo} alt='Logo' height={40} loading='eager' />
+        <Image src='/img/logo.webp' alt='Logo' height={40} width={40} loading='eager' />
       </Link>
       <Suspense>
         <Session />
