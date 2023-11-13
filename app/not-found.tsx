1f58a8f2d63e7aec66bffbfd8eeb10f1d5870d1a
Rafaella Fernández <54990883+chanmi-07@users.noreply.github.com>
2023-11-12 21:13:59 -0500

    Update not-found page 🐱

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div>
-      <Link href='/'>Return Home</Link>
+    <div className='px-5 py-4 w-full h-screen flex flex-col items-center justify-center'>
+      <h1 className='font-bold text-primary text-9xl'>404</h1>
+      <h3 className='font-bold text-neutral-800 text-2xl'>Página no encontrada</h3>
+      <Link href='/' className='bg-primary text-white px-4 py-2 rounded-full mt-2'>Volver al Inicio</Link>
     </div>
   )
 }
