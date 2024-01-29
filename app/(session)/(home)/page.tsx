0b971d79f14a2f6da0800e68143a3a5774f55c0e
Jean <JeanChirinos17@gmail.com>
2024-01-29 18:19:45 -0500

    Refactor filter components and comments in page.tsx

--- app/(session)/(home)/page.tsx
+++ app/(session)/(home)/page.tsx
@@ -18,26 +18,26 @@ export default function Page(props: Props) {
     <main className='mx-auto flex w-[1600px] max-w-full animate-fade gap-x-6 px-2 pb-2 animate-duration-200'>
       <aside className='max-h-[calc(100dvh-3rem-var(--header-height)-0.5rem)] w-60 shrink-0 overflow-y-auto pr-10 max-lg:hidden'>
         <section className='space-y-8 *:space-y-3'>
-          <Suspense>
+          <Suspense keyProp={JSON.stringify(searchParams)}>
             <FilterStatus status={status} />
           </Suspense>
-          <Suspense>
+          {/* <Suspense>
             <FilterCategory category={searchParams.category_id} />
           </Suspense>
           {searchParams.category_id && (
             <Suspense>
               <FilterBreeds category_id={searchParams.category_id} />
             </Suspense>
-          )}
+          )} */}
         </section>
       </aside>
       <section className='flex w-full flex-col gap-y-5'>
-        <header className='flex justify-between'>
+        {/* <header className='flex justify-between'>
           <Suspense>
             <FilterPlace />
           </Suspense>
           <Order order={order} />
-        </header>
+        </header> */}
         {/* <Suspense fallback={<PetGridSkeleton />} keyProp={JSON.stringify(searchParams)}>
           <PetGrid searchParams={{ ...restSearchParams, status, order }} />
         </Suspense> */}
