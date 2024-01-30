2420f254a353f0e8176a13a35cd464a17d3a4317
Jean <JeanChirinos17@gmail.com>
2024-01-29 20:57:59 -0500

    Commented out unused code in FilterPlace.tsx

--- app/(session)/(home)/FilterPlace/FilterPlace.tsx
+++ app/(session)/(home)/FilterPlace/FilterPlace.tsx
@@ -1,23 +1,23 @@
-import { getPlaces } from '@/controllers/Place'
+// import { getPlaces } from '@/controllers/Place'
 // import { FilterPlaceClient } from './FilterPlaceClient'
 // import { waitFor } from '@/utilities/utilities'
 
 export async function FilterPlace() {
   // await waitFor(3)
-  const { departamentos, provincias, distritos } = await getPlaces()
+  // const { departamentos, provincias, distritos } = await getPlaces()
 
-  const allProvincias = Object.values(provincias).flat()
+  // const allProvincias = Object.values(provincias).flat()
 
-  const allDistritos = Object.values(distritos)
-    .flat()
-    .map(d => {
-      const provincia = allProvincias.find(p => p.id_ubigeo === d.id_padre_ubigeo)
-      const departamento = departamentos.find(d => d.id_ubigeo === provincia?.id_padre_ubigeo)
+  // const allDistritos = Object.values(distritos)
+  //   .flat()
+  //   .map(d => {
+  //     const provincia = allProvincias.find(p => p.id_ubigeo === d.id_padre_ubigeo)
+  //     const departamento = departamentos.find(d => d.id_ubigeo === provincia?.id_padre_ubigeo)
 
-      const tag = `${d.etiqueta_ubigeo}, ${departamento?.nombre_ubigeo}`
+  //     const tag = `${d.etiqueta_ubigeo}, ${departamento?.nombre_ubigeo}`
 
-      return { ...d, etiqueta_ubigeo: tag }
-    })
+  //     return { ...d, etiqueta_ubigeo: tag }
+  //   })
 
   return (
     // <FilterPlaceClient
