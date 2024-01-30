6f72ce5a2e6bba946c945eb93562405c89b5475e
Jean <JeanChirinos17@gmail.com>
2024-01-30 00:11:43 -0500

    Refactor FilterPlace component

--- app/(session)/(home)/FilterPlace/FilterPlace.tsx
+++ app/(session)/(home)/FilterPlace/FilterPlace.tsx
@@ -1,4 +1,4 @@
-import { getPlaces, getPlaces2 } from '@/controllers/Place'
+import { getPlaces } from '@/controllers/Place'
 import { FilterPlaceClient } from './FilterPlaceClient'
 // import { FilterPlaceClient } from './FilterPlaceClient'
 
@@ -15,20 +15,20 @@ export async function FilterPlace() {
   //   distritosData,
   // ])
 
-  // console.log({ provincias, distritos })
+  console.log({ departamentos, provincias, distritos })
 
-  const allProvincias = Object.values(provincias).flat()
+  // const allProvincias = Object.values(provincias).flat()
 
-  const allDistritos = Object.values(distritos)
-    .flat()
-    .map(d => {
-      const provincia = allProvincias.find(p => p.id_ubigeo === d.id_padre_ubigeo)
-      const departamento = departamentos.find(d => d.id_ubigeo === provincia?.id_padre_ubigeo)
+  // const allDistritos = Object.values(distritos)
+  //   .flat()
+  //   .map(d => {
+  //     const provincia = allProvincias.find(p => p.id_ubigeo === d.id_padre_ubigeo)
+  //     const departamento = departamentos.find(d => d.id_ubigeo === provincia?.id_padre_ubigeo)
 
-      const tag = `${d.etiqueta_ubigeo}, ${departamento?.nombre_ubigeo}`
+  //     const tag = `${d.etiqueta_ubigeo}, ${departamento?.nombre_ubigeo}`
 
-      return { ...d, etiqueta_ubigeo: tag }
-    })
+  //     return { ...d, etiqueta_ubigeo: tag }
+  //   })
 
   return (
     // <FilterPlaceClient
@@ -36,10 +36,6 @@ export async function FilterPlace() {
     //   provincias={allProvincias}
     //   distritos={allDistritos}
     // />
-    <FilterPlaceClient
-      departamentos={departamentos}
-      provincias={allProvincias}
-      distritos={allDistritos}
-    />
+    <FilterPlaceClient departamentos={[]} provincias={[]} distritos={[]} />
   )
 }
