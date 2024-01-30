34271bc49eb55c22a00cd4eea338277c608f49e6
Jean <JeanChirinos17@gmail.com>
2024-01-29 21:42:20 -0500

    Commented out Form component in publicar/page.tsx

--- app/(session)/publicar/page.tsx
+++ app/(session)/publicar/page.tsx
@@ -5,11 +5,11 @@ export default function Page() {
   return (
     <main className='mx-auto w-[768px] max-w-full animate-fade px-2 py-4 animate-duration-200'>
       <h2 className='mb-10 text-center text-lg font-semibold'>Registro de datos</h2>
-      <Form
+      {/* <Form
         StatusComponent={<Status />}
         PlaceComponent={<Place />}
         CategoryComponent={<Categories />}
-      />
+      /> */}
     </main>
   )
 }
