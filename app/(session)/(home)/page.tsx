9eec3678cfa9d95b6030c3aada9752415c4e4a31
Jean <JeanChirinos17@gmail.com>
2024-01-30 16:42:27 -0500

    Commented out unused code and added links for filter status

--- app/(session)/(home)/page.tsx
+++ app/(session)/(home)/page.tsx
@@ -2,12 +2,13 @@
 import { Suspense, memo } from 'react'
 import { TGetPetParams } from '@/controllers/Pet'
 import { PetGridSkeleton } from '@/Skeletons/PetGridSkeleton'
-import { FilterStatus } from './FilterStatus'
+// import { FilterStatus } from './FilterStatus'
 import { PetGrid } from './Pets'
-import { FilterCategory } from './FilterCategory'
+// import { FilterCategory } from './FilterCategory'
 // import { FilterBreeds } from './FilterBreeds/FilterBreeds'
-import { Order } from './Order'
+// import { Order } from './Order'
 import { FilterPlace } from './FilterPlace/FilterPlace'
+import Link from 'next/link'
 // import dynamic from 'next/dynamic'
 // import { FilterPlaceSkeleton } from '@/Skeletons/FilterPlaceSkeleton'
 
@@ -27,24 +28,24 @@ export default function Page(props: Props) {
   const { status = '1', order = 'asc', ...restSearchParams } = searchParams
 
   return (
-    <main
-      className='mx-auto flex w-[1600px] max-w-full animate-fade gap-x-6 px-2 pb-2 animate-duration-200'
-      key='e'
-    >
+    <main className='mx-auto flex w-[1600px] max-w-full animate-fade gap-x-6 px-2 pb-2 animate-duration-200'>
       <aside className='max-h-[calc(100dvh-3rem-var(--header-height)-0.5rem)] w-60 shrink-0 overflow-y-auto pr-10 max-lg:hidden'>
         <section className='space-y-8 *:space-y-3'>
           {/* <Suspense keyProp={'status' + JSON.stringify(searchParams)}> */}
           {/* <Suspense key={'status' + JSON.stringify(searchParams)}> */}
-          <Suspense>
+          {/* <Suspense>
             <FilterStatus status={status} />
-          </Suspense>
-          <Suspense
+          </Suspense> */}
+          <Link href='?status=1'>Status 1</Link>
+          <Link href='?status=2'>Status 2</Link>
+          <Link href='?status=3'>Status 3</Link>
+          {/* <Suspense
           // keyProp={JSON.stringify(searchParams)}
           // key={JSON.stringify(searchParams)}
           // fallback={<div className='aspect-square w-4 bg-red-500' />}
           >
             <FilterCategory category={searchParams.category_id} />
-          </Suspense>
+          </Suspense> */}
           {/* {searchParams.category_id && (
             <Suspense>
               <FilterBreeds category_id={searchParams.category_id} />
@@ -54,10 +55,10 @@ export default function Page(props: Props) {
       </aside>
       <section className='flex w-full flex-col gap-y-5'>
         <header className='flex justify-between'>
-          {/* <Suspense key='1'> */}
-          <FilterPlaceMemo key='a' />
-          {/* </Suspense> */}
-          <Order order={order} />
+          <Suspense>
+            <FilterPlaceMemo />
+          </Suspense>
+          {/* <Order order={order} /> */}
         </header>
         <Suspense
           fallback={<PetGridSkeleton />}
