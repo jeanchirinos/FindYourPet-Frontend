1bb8eec62137720fe504563a3832b8a0c4b50493
Jean <JeanChirinos17@gmail.com>
2024-02-01 15:21:00 -0500

    Remove and add ignored files in .vscode/settings.json

---
+++ app/adios/example-client-component.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useCallback } from 'react'
+
+export default function ExampleClientComponent() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  return (
+    <>
+      <p>Sort By</p>
+
+      {/* using useRouter */}
+      <button
+        onClick={() => {
+          // <pathname>?sort=asc
+          router.push(pathname + '?' + createQueryString('sort', 'asc'))
+        }}
+      >
+        ASC
+      </button>
+
+      {/* using <Link> */}
+      <Link
+        href={
+          // <pathname>?sort=desc
+          pathname + '?' + createQueryString('sort', 'desc')
+        }
+      >
+        DESC
+      </Link>
+    </>
+  )
+}
---
+++ app/adios/page.tsx
@@ -0,0 +1,9 @@
+import ExampleClientComponent from './example-client-component'
+
+export default function Page() {
+  return (
+    <>
+      <ExampleClientComponent />
+    </>
+  )
+}
