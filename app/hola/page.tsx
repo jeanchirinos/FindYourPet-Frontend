a5b6e3d3a5a16c6d7f5c93735a5ca45606347483
Jean <JeanChirinos17@gmail.com>
2024-02-01 12:19:12 -0500

    Refactor Page component and add status prop

--- app/hola/page.tsx
+++ app/hola/page.tsx
@@ -1,11 +1,21 @@
+import { PageProps } from '@/types'
 import Link from 'next/link'
 
-export default function Page() {
+type Props = PageProps<'', 'status'>
+
+export default function Page(props: Props) {
+  const { status } = props.searchParams
+
+  console.log('status', status)
+
   return (
-    <div className='m-10 flex gap-x-5 *:border *:border-neutral-500 *:p-10 active:*:bg-red-500'>
-      <Link href='?status=1'>1</Link>
-      <Link href='?status=2'>2</Link>
-      <Link href='?status=3'>3</Link>
+    <div className='flex flex-col gap-y-5'>
+      <h1>{status}</h1>
+      <div className='m-10 flex gap-x-5 *:border *:border-neutral-500 *:p-10 active:*:bg-red-500'>
+        <Link href='?status=1'>1</Link>
+        <Link href='?status=2'>2</Link>
+        <Link href='?status=3'>3</Link>
+      </div>
     </div>
   )
 }
