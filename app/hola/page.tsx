fb1848b55456d6747ea27fe08e515307254346a7
Jean <JeanChirinos17@gmail.com>
2024-02-01 12:32:53 -0500

    Refactor page component to use Suspense for loading state

--- app/hola/page.tsx
+++ app/hola/page.tsx
@@ -1,21 +1,20 @@
 import { PageProps } from '@/types'
 import Link from 'next/link'
+import { Suspense } from 'react'
 
 type Props = PageProps<'', 'status'>
 
 export default function Page(props: Props) {
-  const { status } = props.searchParams
-
-  console.log('status', status)
-
   return (
-    <div className='flex flex-col gap-y-5'>
-      <h1>{status}</h1>
-      <div className='m-10 flex gap-x-5 *:border *:border-neutral-500 *:p-10 active:*:bg-red-500'>
-        <Link href='?status=1'>1</Link>
-        <Link href='?status=2'>2</Link>
-        <Link href='?status=3'>3</Link>
+    <Suspense fallback='LOADING' key={props.searchParams.status}>
+      <div className='flex flex-col gap-y-5'>
+        <h1>{props.searchParams.status}</h1>
+        <div className='m-10 flex gap-x-5 *:border *:border-neutral-500 *:p-10 active:*:bg-red-500'>
+          <Link href='/hola?status=1'>1</Link>
+          <Link href='/hola?status=2'>2</Link>
+          <Link href='/hola?status=3'>3</Link>
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
