87d623fa983bf487796dc675f98ef7cd2c81557f
Jean <JeanChirinos17@gmail.com>
2024-02-05 12:30:55 -0500

    Refactor FilterStatusClient component

--- src/app/(session)/(home)/FilterStatus/FilterStatusClient.tsx
+++ src/app/(session)/(home)/FilterStatus/FilterStatusClient.tsx
@@ -4,18 +4,19 @@ import { StatusList } from '@/models/Pet'
 import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import { useCallback, useEffect, useState } from 'react'
+import { Button } from '@nextui-org/react'
 
 type Props = { statusList: StatusList; status: string }
 
 export function FilterStatusClient(props: Props) {
-  const { status } = props
+  // const { status } = props
 
   const searchParams = useSearchParams()
-  const [currentStatus, setCurrentStatus] = useState(status)
+  // const [currentStatus, setCurrentStatus] = useState(status)
 
-  useEffect(() => {
-    setCurrentStatus(status)
-  }, [status])
+  // useEffect(() => {
+  //   setCurrentStatus(status)
+  // }, [status])
 
   const createQueryString = useCallback(
     (id: number) => {
@@ -29,22 +30,31 @@ export function FilterStatusClient(props: Props) {
     [searchParams],
   )
 
-  const isSelected = (id: number) => id.toString() === currentStatus
+  // const isSelected = (id: number) => id.toString() === currentStatus
+  const isSelected = (id: number) => id.toString() === (searchParams.get('status') ?? '1')
 
   return (
     <div className='flex flex-col gap-y-2'>
       {props.statusList.map(item => (
-        <Link
+        // <Link
+        <Button
+          as={Link}
           key={item.id}
           href={createQueryString(item.id)}
-          onClick={() => setCurrentStatus(item.id.toString())}
+          // onClick={() => setCurrentStatus(item.id.toString())}
+          // className={twJoin(
+          //   isSelected(item.id) ? 'bg-orange-100 text-orange-600' : 'hover:bg-foreground-100',
+          //   'flex w-full min-w-max cursor-pointer items-center rounded-lg p-2.5 text-sm shadow-md',
+          // )}
           className={twJoin(
-            isSelected(item.id) ? 'bg-orange-100 text-orange-600' : 'hover:bg-foreground-100',
-            'flex w-full min-w-max cursor-pointer items-center rounded-lg p-2.5 text-sm shadow-md',
+            isSelected(item.id)
+              ? 'bg-orange-100 text-orange-600'
+              : 'bg-transparent hover:bg-foreground-100',
+            'flex w-full min-w-max cursor-pointer items-center justify-start rounded-lg p-2.5 text-sm shadow-md',
           )}
         >
           {item.value}
-        </Link>
+        </Button>
       ))}
     </div>
   )
