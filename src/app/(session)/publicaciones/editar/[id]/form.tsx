17f4a05e5551d072616ca9711dcccf3847f500ce
Jean <JeanChirinos17@gmail.com>
2024-03-08 21:46:16 -0500

    Fix PetImage initialImage property

--- src/app/(session)/publicaciones/editar/[id]/form.tsx
+++ src/app/(session)/publicaciones/editar/[id]/form.tsx
@@ -100,7 +100,7 @@ export function Form(props: Props) {
         }}
         className='flex items-center gap-4 max-md:flex-col md:items-start'
       >
-        <PetImage initialImage={pet.image} />
+        <PetImage initialImage={pet.image.image} />
         <section className='flex w-[400px] max-w-full shrink-0 flex-col gap-y-3'>
           {props.StatusInfoComponent}
           <Textarea
