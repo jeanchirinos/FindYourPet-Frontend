a68d20ed8395e02dd1c3736c2c48e022a76744ae
Jean <JeanChirinos17@gmail.com>
2024-03-23 22:56:06 -0500

    Add authIsOptional flag to getPetById API call

--- src/controllers/PetController/getPetById.ts
+++ src/controllers/PetController/getPetById.ts
@@ -6,6 +6,7 @@ import { getData } from '@/utilities/actionRequest'
 export async function getPetById(id: string | number) {
   const data = await getData<{ pet: Pet }>(`pet-find/${id}`, {
     cache: 'no-store',
+    authIsOptional: true,
   })
 
   return data
