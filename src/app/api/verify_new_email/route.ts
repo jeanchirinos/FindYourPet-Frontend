a5c122f433823741308ea07f3728efb25209ad3e
Jean <JeanChirinos17@gmail.com>
2024-03-30 23:20:20 -0500

    Update verify_new_email route to handle token and cookie parameters

--- src/app/api/verify_new_email/route.ts
+++ src/app/api/verify_new_email/route.ts
@@ -5,12 +5,18 @@ import { type NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
-  const token = searchParams.get('cookie')
+  const cookie = searchParams.get('cookie')
+  const token = searchParams.get('token')
 
-  const expires = new Date()
-  expires.setDate(expires.getDate() + 7)
+  // const expires = new Date()
+  // expires.setDate(expires.getDate() + 7)
 
-  cookies().set('jwt', token as string, { expires })
+  // cookies().set('jwt', token as string, { expires })
 
-  redirect(ROUTE.PETS.INDEX)
+  // redirect(ROUTE.PETS.INDEX)
+  return Response.json({
+    cookie,
+    token,
+    searchParams: searchParams.toString(),
+  })
 }
