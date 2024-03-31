2b42af8b095e234ab8d3244a3c1dc36903140441
Jean <JeanChirinos17@gmail.com>
2024-03-30 23:05:24 -0500

    Refactor GET function in verify_new_email/route.ts

--- src/app/api/verify_new_email/route.ts
+++ src/app/api/verify_new_email/route.ts
@@ -1,16 +1,16 @@
 import { ROUTE } from '@/routes'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { type NextRequest } from 'next/server'
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-
-  const { token } = Object.fromEntries(searchParams)
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const token = searchParams.get('token')
 
   const expires = new Date()
   expires.setDate(expires.getDate() + 7)
 
-  cookies().set('jwt', token, { expires })
+  cookies().set('jwt', token as string, { expires })
 
   redirect(ROUTE.PETS.INDEX)
 }
