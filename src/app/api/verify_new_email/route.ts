a16f4991292bfec6b4cdc14803afe3cc4784f35b
Jean <JeanChirinos17@gmail.com>
2024-03-30 23:17:18 -0500

    Update token parameter in GET request

--- src/app/api/verify_new_email/route.ts
+++ src/app/api/verify_new_email/route.ts
@@ -5,12 +5,12 @@ import { type NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
-  const token = searchParams.get('token')
+  const token = searchParams.get('cookie')
 
   const expires = new Date()
   expires.setDate(expires.getDate() + 7)
 
-  cookies().set('jwt', token as string, { expires, domain: 'petcontrado.nijui.com' })
+  cookies().set('jwt', token as string, { expires })
 
   redirect(ROUTE.PETS.INDEX)
 }
